1d6ac59e9afdbb96266ca87afc7067e8cc28fe7e
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-14 17:48:06 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,5 @@
 def unsafe?(speed)
-  if speed < 40
-    "true"
-    elsif  speed > 60
+  if (speed < 40 || speed > 60)
     "true"
   else
     puts "false"
@@ -13,7 +11,7 @@ end
 
 
 def not_safe?(speed)
-  speed < 40 || > 60 ? "true" : "false"
+  (speed < 40 || > 60) ? "true" : "false"
 
 end
 	
