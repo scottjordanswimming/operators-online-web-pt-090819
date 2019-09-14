1bf5c82d23bd385a9618bb5dbbc02e5c1d9003cb
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-14 17:41:36 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,17 @@
 def unsafe?(speed)
+  if speed < 40 || > 60
+    puts true 
+  else
+    puts false 
+  end
+    
 
 end
 
 
 
 def not_safe?(speed)
+  speed < 40 || > 60 ? true : false 
 	
 end
 	
